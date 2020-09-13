726d38b71d71970a9b9a32c5f6e4462cbf3f8db5
JamesMorales04 <Jamesmoralesmoreno@gmail.com>
2020-09-13 12:28:42 -0500

    adding add users view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import CreateUser from "./components/CreateUser"
+import Navigation from "./components/Navigation"
+import UserList from "./components/UserList"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <div className="container p-4">
+        <Route path="/" exact component={UserList}/>
+        <Route path="/edit/:id" component={CreateUser}/>
+        <Route path="/create" component={CreateUser}/>
+      </div>
+    </Router>
   );
 }
 
